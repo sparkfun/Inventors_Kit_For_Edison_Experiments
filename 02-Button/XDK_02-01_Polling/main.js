f4bba6400f6e844ff4cce6e79c41e48c74810f93
ShawnHymel <shawn.hymel@sparkfun.com>
2015-10-27 17:00:26 -0600

    Added exp 02-01 Polling

---
+++ 02-Button/XDK_02-01_Polling/main.js
@@ -0,0 +1,35 @@
+/**
+ * SparkFun Inventor's Kit for Edison
+ * Experiment 2 - Part 1: Pushing Some Buttons
+ * This sketch was written by SparkFun Electronics
+ * October 27, 2015
+ * https://github.com/sparkfun
+ *
+ * This is a simple example program that prints some text to the console
+ * every time a button is pushed.
+ *
+ * Released under the MIT License(http://opensource.org/licenses/MIT)
+ */
+
+// Import the MRAA module
+var mraa = require('mraa');
+
+// Set up a digital input/output on MRAA pin 36 (GP14)
+var buttonPin = new mraa.Gpio(36);
+
+// Set that pin as a digital input (read)
+buttonPin.dir(mraa.DIR_IN);
+
+// Call the periodicActivity function
+periodicActivity();
+
+// This function is called forever (due to the setTimeout() function)
+function periodicActivity() //
+{
+    // Read the value of the pin and print it to the screen
+    var val = buttonPin.read();
+    console.log('Button is ' + val);
+
+    // Wait for 250 ms and call this function again
+    setTimeout(periodicActivity, 250);
+}
