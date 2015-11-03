5d7e720616756731c4eefd0a1c04971763a900ba
ShawnHymel <shawn.hymel@sparkfun.com>
2015-11-02 22:48:43 -0700

    Added exp 5 parts 1 and 2

---
+++ 05-WebPage/XDK_05-01_Web_Page/main.js
@@ -0,0 +1,41 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
+
+/**
+ * SparkFun Inventor's Kit for Edison
+ * Experiment 5 - Part 1: Web Page
+ * This sketch was written by SparkFun Electronics
+ * November 1, 2015
+ * https://github.com/sparkfun
+ *
+ * Serves a very simple web page from the Edison.
+ *
+ * Released under the MIT License(http://opensource.org/licenses/MIT)
+ */
+
+// Import the HTTP module
+var http = require('http');
+
+// Which port we should connect to
+var port = 4242;
+
+// Create a web server that serves a simple web page
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.write(" <html>                                                      \
+                <head>                                                      \
+                    <title>My Page</title>                                  \
+                </head>                                                     \
+                <body>                                                      \
+                    <p>This is my page. There are many like it,             \
+                    but this one is mine.</p>                               \
+                </body>                                                     \
+                </html>");
+    res.end();
+});
+
+// Run the server on a particular port
+server.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
---
+++ 05-WebPage/XDK_05-02_Web_Button/main.js
@@ -0,0 +1,73 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
+
+/**
+ * SparkFun Inventor's Kit for Edison
+ * Experiment 5 - Part 2: Web Page Button
+ * This sketch was written by SparkFun Electronics
+ * November 1, 2015
+ * https://github.com/sparkfun
+ *
+ * Serves a web page that allows users to turn an LED on and off remotely.
+ *
+ * Released under the MIT License(http://opensource.org/licenses/MIT)
+ */
+
+// Import the MRAA and HTTP modules
+var mraa = require('mraa');
+var http = require('http');
+
+// Set up a digital output on MRAA pin 20 (GP12) for the LED
+var ledPin = new mraa.Gpio(20);
+ledPin.dir(mraa.DIR_OUT);
+ledPin.write(0);
+
+// Global LED variable to know if the LED should be on or off
+var led = 0;
+
+// Which port we should connect to
+var port = 4242;
+
+// Create a web server that serves a simple web page with a button
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.write(" <html>                                                      \
+                <head>                                                      \
+                    <title>LED Controller</title>                           \
+                    <script src='/socket.io/socket.io.js'></script>         \
+                    </script>                                               \
+                </head>                                                     \
+                <body>                                                      \
+                    <p><button onclick='toggle()'>TOGGLE</button></p>       \
+                    <script>                                                \
+                        var socket = io.connect('http://" + 
+                            req.socket.address().address + ":" + 
+                            port + "');                                     \
+                        function toggle() {                                 \
+                            socket.emit('toggle');                          \
+                        }                                                   \
+                    </script>                                               \
+                </body>                                                     \
+                </html>");
+    res.end();
+});
+
+// Listen for a socket connection
+var io = require('socket.io').listen(server);
+
+// Wait for a client to connect
+io.on('connection', function(socket) {
+    console.log('A client is connected!');
+    
+    // Look for the "toggle" message from the client, and toggle the LED
+    socket.on('toggle', function() {
+        led = led ? 0 : 1;
+        ledPin.write(led);
+    });
+});
+
+// Run the server on a particular port
+server.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
