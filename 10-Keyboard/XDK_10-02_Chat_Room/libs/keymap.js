7f5508da7cdd8001c8e6e5c6bb9860c4ffc74caf
ShawnHymel <shawn.hymel@sparkfun.com>
2015-11-20 16:52:32 -0700

    Added 10-02 chat room example.

---
+++ 10-Keyboard/XDK_10-02_Chat_Room/libs/keymap.js
@@ -0,0 +1,63 @@
+// Puts all key presses into a lookup table
+module.exports = {
+    keys: [     // code key
+        null,   // 0    reserved
+        null,   // 1    esc
+        "1",    // 2    1
+        "2",    // 3    2
+        "3",    // 4    3
+        "4",    // 5    4
+        "5",    // 6    5
+        "6",    // 7    6
+        "7",    // 8    7
+        "8",    // 9    8
+        "9",    // 10   9
+        "0",    // 11   0
+        "-",    // 12   minus
+        "=",    // 13   equal
+        "bksp", // 14   backspace
+        null,   // 15   tab
+        "q",    // 16   q
+        "w",    // 17   w
+        "e",    // 18   e
+        "r",    // 19   r
+        "t",    // 20   t
+        "y",    // 21   y
+        "u",    // 22   u
+        "i",    // 23   i
+        "o",    // 24   o
+        "p",    // 25   p
+        "[",    // 26   left brace
+        "]",    // 27   right brace
+        "enter",// 28   enter
+        null,   // 29   left ctrl
+        "a",    // 30   a
+        "s",    // 31   s
+        "d",    // 32   d
+        "f",    // 33   f
+        "g",    // 34   g
+        "h",    // 35   h
+        "j",    // 36   j
+        "k",    // 37   k
+        "l",    // 38   l
+        ";",    // 39   semicolon
+        "'",    // 40   apostrophe
+        "`",    // 41   grave
+        null,   // 42   left shift
+        "\\",   // 43   backslash
+        "z",    // 44   z
+        "x",    // 45   x
+        "c",    // 46   c
+        "v",    // 47   v
+        "b",    // 48   b
+        "n",    // 49   n
+        "m",    // 50   m
+        ",",    // 51   comma
+        ".",    // 52   dot
+        "/",    // 53   slash
+        null,   // 54   right shift
+        null,   // 55   kpasterisk
+        null,   // 56   left alt
+        " "     // 57   space
+    ]
+};
---
+++ 10-Keyboard/XDK_10-02_Chat_Room/main.js
@@ -0,0 +1,152 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
+
+/**
+ * SparkFun Inventor's Kit for Edison
+ * Experiment 10 - Part 2: Chat Room
+ * This sketch was written by SparkFun Electronics
+ * November 20, 2015
+ * https://github.com/sparkfun
+ *
+ * Serves a chat room where users can post messages. Captures keyboard input
+ * and posts messages to the chat room.
+ *
+ * Released under the MIT License(http://opensource.org/licenses/MIT)
+ */
+
+// Import the filesystem module and our keymap table
+var fs = require('fs');
+var keymap = require('./libs/keymap.js');
+
+// We'll also need johny-five and its Edison wrapper
+var five = require('johnny-five');
+var Edison = require('edison-io');
+var board = new five.Board({
+    io: new Edison()
+});
+
+// Import HTTP and Express modules
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Global variables
+var port = 4242;
+var lcd;
+
+// Johnny Five initialization
+board.on('ready', function() {
+    
+    // Create our LCD object and define the pins
+    // LCD pin name:    RS  EN DB4 DB5 DB6 DB7
+    // Edison GPIO:     14  15  44  45  46  47
+    lcd = new five.LCD({
+        pins: ["GP14", "GP15", "GP44", "GP45", "GP46", "GP47"],
+        backlight: 6,
+        rows: 2,
+        cols: 16
+    });
+    
+    // Turn on LCD, clear it, and set cursor to home
+    lcd.on();
+    lcd.clear();
+    lcd.home();
+    lcd.blink();
+    lcd.cursorPos = 0;
+    lcd.msg = "";
+    console.log("Start typing!");
+});
+
+// Create a stream that emits events on every key stroke
+var readableStream = fs.createReadStream('/dev/input/event2');
+
+// Callback for a key event
+readableStream.on('data', function(buf) {
+    
+    // Check for key down event and determine key pressed
+    if ((buf[24] == 1) && (buf[28] == 1)) {
+        var keyCode = ((buf[27] & 0xff) << 8) | (buf[26] & 0xff);
+        var keyChar = keymap.keys[keyCode];
+        
+        // Make the character appear on the LCD
+        if (lcd !== undefined) {
+            
+            // If it is a backspace, delete the previous character
+            if (keyChar === 'bksp') {
+                if (lcd.msg !== "") {
+                    lcd.msg = lcd.msg.slice(0, -1);
+                }
+                lcd.cursorPos--;
+                if (lcd.cursorPos <= 0) {
+                    lcd.cursorPos = 0;
+                }
+                lcd.print(" ");
+                lcd.cursor(
+                    Math.floor(lcd.cursorPos / lcd.cols), 
+                    (lcd.cursorPos % lcd.cols)
+                );
+                lcd.print(" ");
+                lcd.cursor(
+                    Math.floor(lcd.cursorPos / lcd.cols), 
+                    (lcd.cursorPos % lcd.cols)
+                );
+                
+            // If it is a return character, post message and clear the LCD
+            } else if (keyChar == 'enter') {
+                console.log("Server: " + lcd.msg);
+                io.emit('chat message', "Server: " + lcd.msg);
+                lcd.clear();
+                lcd.cursorPos = 0;
+                lcd.msg = "";
+                
+            // Otherwise, print the character to the LCD
+            } else if ((keyChar !== null) && (keyChar !== undefined)) {
+                
+                // Have the character appear on the LCD and append to message
+                lcd.print(keyChar);
+                lcd.cursorPos++;
+                
+                // Stop the cursor at the end of the LCD
+                if (lcd.cursorPos >= (lcd.rows * lcd.cols)) {
+                    lcd.cursorPos = (lcd.rows * lcd.cols) - 1;
+                }
+                
+                // Remove the last char if we reached the end of the buffer
+                if (lcd.msg.length >= (lcd.rows * lcd.cols)) {
+                    lcd.msg = lcd.msg.slice(0, -1);
+                }
+                
+                // Append character to message
+                lcd.msg = lcd.msg.concat(keyChar);
+            }
+            
+            // Update the cursor position (wrap to second line if needed)
+            lcd.cursor(
+                Math.floor(lcd.cursorPos / lcd.cols), 
+                (lcd.cursorPos % lcd.cols)
+            );
+        }
+    }
+});
+
+// Send the web page on client request
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+// Create a handler for when a client connects
+io.on('connection', function(socket) {
+    var clientIP = socket.client.conn.remoteAddress;
+    
+    // If we get a chat message, send it out to all clients
+    socket.on('chat message', function(msg) {
+        console.log(clientIP + ": " + msg);
+        io.emit('chat message', clientIP + ": " + msg);
+    });
+});
+
+// Start the server
+http.listen(4242, function() {
+    console.log('Server listening on port ' + port);
+});
