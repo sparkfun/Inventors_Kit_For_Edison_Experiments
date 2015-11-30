80d53849732bebf1923d6527baf0d53078ee4197
ShawnHymel <shawn.hymel@sparkfun.com>
2015-11-30 16:48:07 -0700

    Slightly more progress on exp 11-phone

--- 11-Accelerometer/XDK_11_Phone/www/js/app.js
+++ 11-Accelerometer/XDK_11_Phone/www/js/app.js
@@ -19,37 +19,110 @@
 // Put in strict mode to restrict some JavaScript "features"
 "use strict" ;
 
+// BLE service and characteristic information
+window.edison = {
+    deviceId: "98:4F:EE:04:3E:F9",
+    service: "12ab",
+    characteristic: "34cd",
+    connected: false
+};
+
+/******************************************************************************
+ * Bluetooth connection
+ *****************************************************************************/
+
+// Global app object we can use to create BLE callbacks
+window.app = {
+    
+    // Call this first!
+    initialize: function() {
+        window.app.connect();
+    },
+    
+    // Scan for and connect to our statically-encoded Edison MAC address
+    connect: function() {
+        ble.scan([], 
+                 5,
+                 window.app.onDiscoverDevice,
+                 window.app.onError);
+    },
+    
+    // Find BLE devices in range and connect to the Edison
+    onDiscoverDevice: function(device) {
+        debug("Found " + device.name + " at " + device.id);
+        if (device.id === window.edison.deviceId) {
+            debug("Connecting to: " + window.edison.deviceId);
+            ble.connect(window.edison.deviceId,
+                        window.app.onConnect,
+                        window.app.onError);
+        }
+    },
+    
+    //  On BLE connection, notify the user
+    onConnect: function() {
+        debug("Connected to " + window.edison.deviceId);
+        window.edison.connected = true;
+    },
+    
+    // Alert the user if there is an error
+    onError: function(err) {
+        window.edison.connected = false;
+        debug("Error: " + err);
+        alert("Error: " + err);
+    }
+};
+
+/******************************************************************************
+ * Execution starts here after the phone has finished initializing
+ *****************************************************************************/
+
+// Wait for the device (phone) to be ready before connecting to the Edison
+// and polling the accelerometer
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
+    
+    // Prepare the BLE connection
+    window.app.initialize();
+    
+    // Poll the accelerometer every 50 ms and send that data to the Edison
     setInterval(function() {
         navigator.accelerometer.getCurrentAcceleration(function(accel) {
-            debug("X:" + accel.x + " Y:" + accel.y + " Z:" + accel.z +
-                  " T:" + accel.timestamp);
+            
+            // Create an array of accelerometer values
+            var a = [accel.x, accel.y, accel.z];
+            
+            // Set new values for X, Y, and Z acceleration on phone
+            $('#x')[0].innerHTML = a[0].toFixed(2);
+            $('#y')[0].innerHTML = a[1].toFixed(2);
+            $('#z')[0].innerHTML = a[2].toFixed(2);
+            
+            // Scale acceleration data to 16-bit, signed integers
+            a.forEach(function(value, index, arr) {
+                value = Math.trunc(value * 100);
+                value = Math.min(value, 32767);
+                arr[index] = Math.max(value, -32768);
+            });
+            
+            // If we have a BLE connection, send out the accelerometer data
+            debug("checking");
+            if (window.edison.connected) {
+                debug("Writing acc data...");
+                /*ble.write(window.edison.deviceId,
+                          window.edison.service,
+                          window.edison.characterisstic,
+                          Int16Array.from(a),
+                          function() {
+                    debug("Acc data written!");
+                }, window.app.onError);*/
+            } else {
+                debug("NOT connected");
+            }
+            
         }, function() {
             debug("Error reading accelerometer");
         });
-    }, 500);
+    }, 50);
 }
-
-/******************************************************************************
- * Execution starts here after page has loaded
- *****************************************************************************/
-
-// Short for jQuery(document).ready() method, which is called after the page
-// has loaded. We can use this to assign callbacks to elements on the page.
-$(function() {
-    
-    // Initialize the app and assign callbacks
-    debug("test");
-    
-    // Read the stuff
-    /*navigator.accelerometer.getCurrentAcceleration(function(accel) {
-        debug("X:" + accel.x + " Y:" + accel.y + " Z:" + accel.z +
-              " T:" + accel.timestamp);
-    }, function() {
-        debug("Error reading accelerometer");
-    });*/
-});
 
 // Create a pseudo-debugging console
 // NOTE: Real apps should use alert(), but list messages can be useful when
